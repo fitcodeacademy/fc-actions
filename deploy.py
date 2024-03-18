4c30407b960f33ae09a9d87cd817d3a7e7fa2301
iserdyukov <serdiukov47@gmail.com>
2024-03-18 10:42:53 +0200

    deploy 10

--- deploy.py
+++ deploy.py
@@ -1,8 +1,13 @@
+import os
 import ftputil
 
 def ftp_upload(hostname, username, password, remote_dir, local_dir):
     with ftputil.FTPHost(hostname, username, password) as host:
-        host.upload(local_dir, remote_dir)
+        for root, dirs, files in os.walk(local_dir):
+            for filename in files:
+                local_path = os.path.join(root, filename)
+                remote_path = os.path.join(remote_dir, os.path.relpath(local_path, local_dir))
+                host.upload(local_path, remote_path)
 
 if __name__ == "__main__":
     import sys
